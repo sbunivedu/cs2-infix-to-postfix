94fa061e851e046e25618949262c75726748b821
Baochuan Lu <lubaochuan@gmail.com>
2019-09-16 15:55:42 -0500

    initial checkin

---
+++ Tester.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tester{
+  /**
+   * Reads and evaluates multiple postfix expressions.
+   */
+  public static void main(String[] args){
+    String expression, again;
+    String result;
+  
+    Scanner in = new Scanner(System.in);
+    InfixToPostfixConverter converter = new InfixToPostfixConverter();
+    System.out.println(converter.convert("a + b"));
+    System.out.println(converter.convert("2 + 3 * 4"));
+    System.out.println(converter.convert("( a + b ) * ( c - d )"));
+    System.out.println(converter.convert("A + B * C - D"));
+  }
+}
